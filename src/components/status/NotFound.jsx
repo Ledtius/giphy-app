058e4b9e58347de2399ae19d58381c44ed41eed5
Calet Devia <caletdevia@gmail.com>
2026-02-26 19:44:54 -0500

    Added a wait stateVariable to control when the NotFound.jsx shows at the begin of a query even if this condition in a few seconds doesn't matches

--- src/components/status/NotFound.jsx
+++ src/components/status/NotFound.jsx
@@ -1,31 +1,49 @@
 import { motion } from "motion/react";
 import notFound from "../../assets/not-found.svg";
+import { useEffect, useState } from "react";
 
 export const NotFound = (containerAnimation, childrenAnimation) => {
+  const [wait, setWait] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setWait(true);
+    }, 1000);
+  }, []);
+
   return (
     <motion.div
       className="basic-alight"
       key="empty"
       variants={containerAnimation}
-      initial="hidden"
-      animate="visible"
-      exit="out"
+      initial={{ opacity: 0 }}
+      animate={{
+        opacity: 1,
+        transition: { staggerChildren: 0.1, delayChildren: 0.1 },
+      }}
+      exit={{ opacity: 0 }}
     >
-      <motion.img
-        variants={childrenAnimation}
-        initial="hidden"
-        animate="visible"
-        src={notFound}
-        className="imgs-speakers"
-      />
-      <motion.p
-        variants={childrenAnimation}
-        initial="hidden"
-        animate="visible"
-        className="font-bold"
-      >
-        Oops! Aqui no hay nada parecido.
-      </motion.p>
+      {wait ? (
+        <>
+          <motion.img
+            variants={childrenAnimation}
+            initial="hidden"
+            animate="visible"
+            src={notFound}
+            className="imgs-speakers"
+          />
+          <motion.p
+            variants={childrenAnimation}
+            initial="hidden"
+            animate="visible"
+            className="font-bold"
+          >
+            Oops! Aqui no hay nada parecido.
+          </motion.p>
+        </>
+      ) : (
+        ""
+      )}
     </motion.div>
   );
 };
